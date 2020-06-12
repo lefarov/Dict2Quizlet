e475615d305347d5e02cbdcd165ff2d60ec88c9b
mlefarov <mlefarov@gmail.com>
2020-06-12 22:51:35 +0200

    Working bookstore example.

--- dict_to_quizlet/client/src/main.js
+++ dict_to_quizlet/client/src/main.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import BoorstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(BoorstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
--- dict_to_quizlet/server/app.py
+++ dict_to_quizlet/server/app.py
@@ -1,6 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
 
 # configuration
 DEBUG = True
@@ -17,6 +35,20 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {"status": "success"}
+    
+    if request.method == "POST":
+        post_data = request.get_json()
+        BOOKS.append(post_data)
+        response_object["message"] = "Books is added!"
+    else:
+        response_object["books"]  = BOOKS 
+
+    return jsonify(response_object)
 
 
 if __name__ == '__main__':
