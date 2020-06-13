6d4d4645921fa58840579fd5849b010cc494d248
mlefarov <mlefarov@gmail.com>
2020-06-13 13:56:43 +0200

    Working book exaple.

--- dict_to_quizlet/server/app.py
+++ dict_to_quizlet/server/app.py
@@ -1,19 +1,24 @@
+
+import uuid
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
 BOOKS = [
     {
+        "id": uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        "id": uuid.uuid4().hex,
         'title': 'Harry Potter and the Philosopher\'s Stone',
         'author': 'J. K. Rowling',
         'read': False
     },
     {
+        "id": uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
@@ -31,6 +36,14 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+def get_book_index(book_id):
+    for i, book in enumerate(BOOKS):
+        if book["id"] == book_id:
+            return i
+
+    raise ValueError("trying to access unexisting book.")
+
+
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
@@ -42,11 +55,24 @@ def all_books():
     response_object = {"status": "success"}
     
     if request.method == "POST":
-        post_data = request.get_json()
-        BOOKS.append(post_data)
-        response_object["message"] = "Books is added!"
+        BOOKS.append({"id": uuid.uuid4().hex, **request.get_json()})
+        response_object["message"] = "Book is added!"
     else:
         response_object["books"]  = BOOKS 
+
+    return jsonify(response_object)
+
+
+@app.route("/books/<book_id>", methods=["PUT", "DELETE"])
+def single_book(book_id):
+    response_object = {"status": "success"}
+    
+    if request.method == "PUT":
+        BOOKS[get_book_index(book_id)].update(request.get_json())
+        response_object["message"] = "Book is updated!"
+    else:
+        BOOKS[:] = [book for book in BOOKS if book["id"] != book_id]
+        response_object["message"] = "Book is removed!"
 
     return jsonify(response_object)
 
