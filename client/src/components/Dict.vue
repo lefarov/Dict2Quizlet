536d0890f51b6a04799fb469e6233743579b4cfe
mlefarov <mlefarov@gmail.com>
2020-06-21 10:27:32 +0200

    Table is scrollable.

--- client/src/components/Dict.vue
+++ client/src/components/Dict.vue
@@ -1,68 +1,73 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-sm-10">
+      <div class="col-sm-12">
         <h1>Dict</h1>
-      </div>
-      <div class="input-group mb-3">
-        <input id="search-term-input"
-               type="text"
-               class="form-control"
-               placeholder="Search term"
-               v-model="query"
-               @keyup.enter="onTranslate">
-        <div class="input-group-append">
-          <button type="button"
-                  class="btn btn-success btn-sm"
-                  @click="onTranslate">
-                  Translate
-          </button>
+        <div class="input-group mb-3">
+          <input id="search-term-input"
+                type="text"
+                class="form-control"
+                placeholder="Search term"
+                v-model="query"
+                @keyup.enter="onTranslate">
+          <div class="input-group-append">
+            <button type="button"
+                    class="btn btn-success btn-sm"
+                    @click="onTranslate">
+                    Translate
+            </button>
+          </div>
+        </div>
+        <div class="panel-body table-responsive">
+          <table class="table table-striped table-sm"
+                v-for="(category, name, index) in translation"
+                :key="index">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">{{ name }}</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(translation, index) in category" :key="index">
+                  <td style="width: 50%">{{ translation.en }}</td>
+                  <td style="width: 50%">{{ translation.de }}</td>
+                </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="input-group mt-3">
+          <input id="selected-term"
+                type="text"
+                class="form-control"
+                v-model="selectedTerm">
+          <input id="selected-translation"
+                type="text"
+                class="form-control"
+                v-model="selectedTranslation">
+          <div class="input-group-append">
+            <button type="button"
+                    class="btn btn-danger btn-sm"
+                    @click="onSelectTranslation">
+                    Clear
+            </button>
+            <button type="button"
+                    class="btn btn-primary btn-sm"
+                    @click="onSelectTranslation">
+                    Submit
+            </button>
+          </div>
         </div>
       </div>
-      <table class="table table-striped table-sm"
-             v-for="(category, name, index) in translation"
-             :key="index">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">{{ name }}</th>
-            <th scope="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(translation, index) in category" :key="index">
-              <td style="width: 50%">{{ translation.en }}</td>
-              <td style="width: 50%">{{ translation.de }}</td>
-            </tr>
-        </tbody>
-      </table>
-      <div class="input-group mb-3">
-        <input id="selected-term"
-               type="text"
-               class="form-control"
-               v-model="selectedTerm">
-        <input id="selected-translation"
-               type="text"
-               class="form-control"
-               v-model="selectedTranslation">
-        <div class="input-group-append">
-          <button type="button"
-                  class="btn btn-danger btn-sm"
-                  @click="onSelectTranslation">
-                  Clear
-          </button>
-          <button type="button"
-                  class="btn btn-primary btn-sm"
-                  @click="onSelectTranslation">
-                  Submit
-          </button>
-        </div>
-      </div>
-      <!-- <textarea name="selected-translation"
-                  class="form-control"
-                  v-model="selectedTranslation"></textarea> -->
     </div>
   </div>
 </template>
+
+<style>
+.table-responsive {
+    max-height:70vh;
+}
+</style>
 
 <script>
 import axios from 'axios';
