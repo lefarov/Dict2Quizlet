f7a3101a23245dd7c54ca99c1c0d8c9d1f92cd37
mlefarov <mlefarov@gmail.com>
2020-06-24 21:21:25 +0200

    Google drive operation are working.

--- server/docs.py
+++ server/docs.py
@@ -31,34 +31,68 @@ def get_creds():
   return creds
 
 
-def create_doc(creds):
-  title = 'Test Document'
-  body = dict(title=title)
-  service = build('docs', 'v1', credentials=creds)
-
-  doc = service.documents().create(body=body).execute()
-  print(f"Created document with title: {doc.get('title')}")
-
-
-def list_docs(folder_name, creds):
-  # TODO: search for files in specified folder as one query
+def get_folder_id(folder_name, creds):
   service = build('drive', 'v3', credentials=creds)
   folder_res = service.files().list(
-    q=f"mimeType='application/vnd.google-apps.folder' and name='{folder_name}'", 
+    q=f"mimeType='application/vnd.google-apps.folder' and name='{folder_name}' and not trashed", 
     fields='files(id)').execute()
   
   folder_ids = folder_res.get('files', [])
-  if folder_ids:
-    folder_id = folder_ids[0].get('id')
-  else:
-    raise ValueError('leo2dict folder is not found in Google Drive')
+  if not folder_ids:
+    raise ValueError(f"{folder_name} folder is not found in Google Drive")
 
+  return folder_ids[0].get('id')
+
+
+def list_docs(folder_id, creds):
+  service = build('drive', 'v3', credentials=creds)
   files = service.files().list(
-    q=f"mimeType = 'application/vnd.google-apps.document' and '{folder_id}' in parents", 
+    q=f"mimeType = 'application/vnd.google-apps.document' and '{folder_id}' in parents and not trashed", 
     fields='files(id, name)').execute()
 
   return files.get('files', [])
 
+
+def create_doc(name, folder_id, creds):
+  service = build('drive', 'v3', credentials=creds)
+  file_metadata = dict(name=name, 
+                       parents=[folder_id],
+                       mimeType='application/vnd.google-apps.document')
+                       
+  doc_file = service.files().create(body=file_metadata,
+                                    fields='id').execute()
+
+  return doc_file.get('id')
+
+
+def append_transalation(translation, doc_id, creds):
+  service = build('docs', 'v1', credentials=creds)
+  document = service.documents().get(documentId=doc_id).execute()
+
+  # Get the last content item to define the insertion index
+  last_content_item = document.get('body').get('content')[-1]
+  # Build the request
+  requests = [
+    { 'insertText': 
+      { 'location': 
+        { 'index': last_content_item.get('startIndex'), },
+        'text': translation
+      }
+    },
+  ]
+
+  result = service.documents().batchUpdate(
+    documentId=doc_id,
+    body={'requests': requests}).execute()
+
+  return result
+
+
 if __name__ == '__main__':
   creds = get_creds()
-  print(list_docs('leo2quizlet', creds))
+  folder_id = get_folder_id('leo2quizlet', creds)
+
+  print(list_docs(folder_id, creds))
+  # doc_id = create_doc('Test from python 1', folder_id, creds)
+  # for i in range(10):
+  #   append_transalation(f"aaaa\t\t{i}\n", doc_id, creds)
